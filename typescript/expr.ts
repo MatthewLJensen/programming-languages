089d36399a9a4e082d5b7160b4dfb4d59f35beae
Matthew Jensen <matthewljensen@gmail.com>
2022-01-23 18:19:07 -0500

    created the AstGenerator and created the stmt.ts and expr.ts files.
    Tested with the AstPrinter class. Didn't fully implement it since it isn't required.

--- typescript/expr.ts
+++ typescript/expr.ts
@@ -1,12 +1,200 @@
-import { Token } from './token';
+import { Token } from "./token"
 
-abstract class Expr {
-    class Binary extends Expr {
-        constructor(left: Expr, operator: Token, right: Expr) {
-            this.left = left
-            this.operator = operator
-            this.right = right
-        }
+ 
+export interface Visitor<R> {
+    visitAssignExpr(expr: Assign): R
+    visitBinaryExpr(expr: Binary): R
+    visitCallExpr(expr: Call): R
+    visitGetExpr(expr: Get): R
+    visitGroupingExpr(expr: Grouping): R
+    visitLiteralExpr(expr: Literal): R
+    visitLogicalExpr(expr: Logical): R
+    visitSetExpr(expr: Set): R
+    visitSuperExpr(expr: Super): R
+    visitThisExpr(expr: This): R
+    visitUnaryExpr(expr: Unary): R
+    visitVariableExpr(expr: Variable): R
+}
+export abstract class Expr {
+    abstract accept<R>(visitor: Visitor<R>): R
+}
 
+export class Assign extends Expr {
+    public name: Token
+    public value: Expr
+
+    constructor(name: Token, value: Expr,    ) {
+        super()
+        this.name = name
+        this.value = value
     }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitAssignExpr(this)
+        }
+}
+
+export class Binary extends Expr {
+    public left: Expr
+    public operator: Token
+    public right: Expr
+
+    constructor(left: Expr, operator: Token, right: Expr,    ) {
+        super()
+        this.left = left
+        this.operator = operator
+        this.right = right
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitBinaryExpr(this)
+        }
+}
+
+export class Call extends Expr {
+    public callee: Expr
+    public paren: Token
+    public args: Expr[]
+
+    constructor(callee: Expr, paren: Token, args: Expr[],    ) {
+        super()
+        this.callee = callee
+        this.paren = paren
+        this.args = args
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitCallExpr(this)
+        }
+}
+
+export class Get extends Expr {
+    public object: Expr
+    public name: Token
+
+    constructor(object: Expr, name: Token,    ) {
+        super()
+        this.object = object
+        this.name = name
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitGetExpr(this)
+        }
+}
+
+export class Grouping extends Expr {
+    public expression: Expr
+
+    constructor(expression: Expr,    ) {
+        super()
+        this.expression = expression
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitGroupingExpr(this)
+        }
+}
+
+export class Literal extends Expr {
+    public value: Object
+
+    constructor(value: Object,    ) {
+        super()
+        this.value = value
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitLiteralExpr(this)
+        }
+}
+
+export class Logical extends Expr {
+    public left: Expr
+    public operator: Token
+    public right: Expr
+
+    constructor(left: Expr, operator: Token, right: Expr,    ) {
+        super()
+        this.left = left
+        this.operator = operator
+        this.right = right
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitLogicalExpr(this)
+        }
+}
+
+export class Set extends Expr {
+    public object: Expr
+    public name: Token
+    public value: Expr
+
+    constructor(object: Expr, name: Token, value: Expr,    ) {
+        super()
+        this.object = object
+        this.name = name
+        this.value = value
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitSetExpr(this)
+        }
+}
+
+export class Super extends Expr {
+    public keyword: Token
+    public method: Token
+
+    constructor(keyword: Token, method: Token,    ) {
+        super()
+        this.keyword = keyword
+        this.method = method
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitSuperExpr(this)
+        }
+}
+
+export class This extends Expr {
+    public keyword: Token
+
+    constructor(keyword: Token,    ) {
+        super()
+        this.keyword = keyword
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitThisExpr(this)
+        }
+}
+
+export class Unary extends Expr {
+    public operator: Token
+    public right: Expr
+
+    constructor(operator: Token, right: Expr,    ) {
+        super()
+        this.operator = operator
+        this.right = right
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitUnaryExpr(this)
+        }
+}
+
+export class Variable extends Expr {
+    public name: Token
+
+    constructor(name: Token,    ) {
+        super()
+        this.name = name
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visitVariableExpr(this)
+        }
 }
---
+++ typescript/tools/generateAst.ts
@@ -0,0 +1,173 @@
+import { argv } from 'process'
+import { writeFileSync } from 'fs'
+
+const dependencies = [
+    'import { Token } from "./token"'
+]
+
+const expressions = [
+    //> Statements and State assign-expr
+    "Assign   : Token name, Expr value",
+    //< Statements and State assign-expr
+    "Binary   : Expr left, Token operator, Expr right",
+    //> Functions call-expr
+    "Call     : Expr callee, Token paren, Expr[] args", //altered from book in 2 ways. Changed List<Expr> to Expr[] and arguments to args
+    //< Functions call-expr
+    //> Classes get-ast
+    "Get      : Expr object, Token name",
+    //< Classes get-ast
+    "Grouping : Expr expression", "Literal  : Object value",
+    //> Control Flow logical-ast
+    "Logical  : Expr left, Token operator, Expr right",
+    //< Control Flow logical-ast
+    //> Classes set-ast
+    "Set      : Expr object, Token name, Expr value",
+    //< Classes set-ast
+    //> Inheritance super-expr
+    "Super    : Token keyword, Token method",
+    //< Inheritance super-expr
+    //> Classes this-ast
+    "This     : Token keyword",
+    //< Classes this-ast
+    /* Representing Code call-define-ast < Statements and State
+       var-expr "Unary    : Token operator, Expr right"
+    */
+    //> Statements and State var-expr
+    "Unary    : Token operator, Expr right",
+    "Variable : Token name"
+]
+
+// this was using Java namespaces. I decided to import the Expr module as Expr, so everything is referenced as Expr., including the abstract Expr, Which looks like Expr.Expr
+const statements = [
+    "Block      : Stmt[] statements", // altered from book.
+    //< block-ast
+    /* Classes class-ast < Inheritance superclass-ast
+          "Class      : Token name, List<Stmt.Function> methods",
+    */
+    //> Inheritance superclass-ast
+    "Class      : Token name, Expr.Variable superclass," // altered from book
+    + " Function[] methods", // altered from book
+    //< Inheritance superclass-ast
+    "Expression : Expr.Expr expression",
+    //> Functions function-ast
+    "Func   : Token name, Token[] params," //altered from book. Function changed to Func
+    + " Stmt[] body", // altered from book
+    //< Functions function-ast
+    //> Control Flow if-ast
+    "If         : Expr.Expr condition, Stmt thenBranch,"
+    + " Stmt elseBranch",
+    //< Control Flow if-ast
+    /* Statements and State stmt-ast < Statements and State
+       var-stmt-ast "Print      : Expr expression"
+    */
+    //> var-stmt-ast
+    "Print      : Expr.Expr expression",
+    //< var-stmt-ast
+    //> Functions return-ast
+    "Return     : Token keyword, Expr.Expr value",
+    //< Functions return-ast
+    /* Statements and State var-stmt-ast < Control Flow while-ast
+          "Var        : Token name, Expr initializer"
+    */
+    //> Control Flow while-ast
+    "Var        : Token name, Expr.Expr initializer",
+    "While      : Expr.Expr condition, Stmt body"
+]
+
+function defineAST(outputDir: string, baseName: string, types: string[]) {
+    let output = ""
+
+    output += dependencies.join('\n')
+
+    if (baseName == "Stmt"){
+        output += '\nimport * as Expr from "./Expr"'
+    }
+
+    //Generate the visitor interfaces
+    output += `\n\n ${defineVisitor(baseName, types)}`
+
+    output += `\nexport abstract class ${baseName} {\n    abstract accept<R>(visitor: Visitor<R>): R\n}\n\n`
+
+    for (const type of types) {
+        const className = type.split(':')[0].trim()
+        const fields = type.split(":")[1].trim()
+        output += defineType(baseName, className, fields)
+    }
+
+    const path = `${baseName}.ts`
+    // we don't need to use outputdir since we are going to store them all together.
+    writeFileSync(path, output.trim(), { flag: 'w' })
+
+
+}
+
+function defineVisitor(baseName: string, types: string[]) {
+    let output: string = `\nexport interface Visitor<R> {\n`
+
+    for (const type of types) {
+        const typeName = type.split(":")[0].trim()
+        output += `    visit${typeName + baseName}(${baseName.toLowerCase()}: ${typeName}): R\n`
+    }
+    output += "}\n"
+
+    return output
+}
+
+function defineType(baseName: string, className: string, fieldList: string) {
+    let output = ""
+
+    output += `export class ${className} extends ${baseName} {\n`
+
+    // class members
+    for (const field of fieldList.split(",")) {
+        const name = field.trim().split(" ")[1]
+        const type = field.trim().split(" ")[0]
+        output += `    public ${name.trim()}: ${type}\n`
+    }
+
+    // constructor
+    output += `\n    constructor(`
+    
+    // constructor parameters
+    for (const field of fieldList.split(",")) {
+        const name = field.trim().split(" ")[1]
+        const type = field.trim().split(' ')[0]
+        output += `${name.trim()}: ${type}, `
+    }
+
+    // trim off last ", " 
+    output = output.substring(0, output.length - 1)
+
+    // assignments
+    output += `    ) {\n        super()\n`
+    for (const field of fieldList.split(",")) {
+        const name = field.trim().split(" ")[1]
+        output += `        this.${name} = ${name}\n`
+    }
+    output += '    }\n\n'
+
+    output += `    accept = <R>(visitor: Visitor<R>): R => {
+            return visitor.visit${className + baseName}(this)
+        }`
+
+    output += '\n}\n\n'
+
+    return output
+
+}
+
+const main = (): void => {
+    const args = argv.slice(2)
+
+    if (args.length !== 0) {
+        console.log("Not specifying correct number of arguments. Usage: generateAST")
+    } else {
+        defineAST(args[0], 'Expr', expressions)
+        defineAST(args[0], 'Stmt', statements)
+    }
+
+}
+
+main()
+
+
